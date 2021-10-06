7e49b298eeff5bf2458ba7db4607bec6a7e0e046
Rathanson <rathanson1993@gmail.com>
2021-10-05 22:47:10 -0700

    done

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,19 @@
+package org.lang;
+
+public class LanguageInfo {
+	public void tamilLanguage() {
+		System.out.println("Tamil");
+
+	}
+
+	public void englishLanguag() {
+		System.out.println("English");
+
+	}
+
+	public void hindiLanguage() {
+		System.out.println("Hindi");
+
+	}
+
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,28 @@
+package org.lang;
+
+import java.awt.Desktop.Action;
+
+public class StateDetails {
+	public void southIndia() {
+		System.out.println("Tamilnadu");
+
+	}
+	public void northIndia() {
+		System.out.println("Mumbai");
+
+	}
+	public static void main(String[] args) {
+		StateDetails sta=new StateDetails();
+		 sta.southIndia();
+		 sta.northIndia();
+	   LanguageInfo lang=new LanguageInfo();
+	   lang.tamilLanguage();
+	   lang.englishLanguag();
+	   lang.hindiLanguage();
+	   
+	   
+	   
+		
+	}
+
+}
